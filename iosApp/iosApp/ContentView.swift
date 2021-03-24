24f6d35b3728c98ef06daebc3c047ff95fb92b2d
Mingyan Bao <ming.bao@gmail.com>
2021-03-24 15:02:16 -0600

    Add iosApp and related configs.

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import shared
+
+func greet() -> String {
+    return Greeting().greeting()
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text(greet())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
